e08bb69757e6d7e0269f681e0fe54ef452d56f88
lufranski <lufranski@gmail.com>
2022-11-08 16:38:22 +0100

    JS Bug

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Calcolo del prezzo del biglietto del treno
+
+// Variabili
+const titolo = document.getElementById("title");
+const nameField = document.getElementById("name");
+const tripField = document.getElementById("trip");
+const ageField = document.getElementById("age");
+const myButton = document.querySelector("button");
+const myButtonReset = document.getElementById("reset");
+
+myButton.addEventListener("click",
+    function(){
+        const nome = nameField.value;
+        const kmViaggio = tripField.value;
+        var prezzo = 0.21 * kmViaggio; 
+        titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km, il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+    }
+);
+
+myButtonReset.addEventListener("click",
+    function(){
+        nameField.value = null;
+        tripField.value = null;
+    }
+);
+
+// Condizioni
+
+if (ageField.value == "under") { //Sconto del 20%
+    const nome = nameField.value;
+    const kmViaggio = tripField.value;
+    const etaPasseggero = ageField.value;
+    var prezzo = 0.21 * kmViaggio; 
+    prezzo -= prezzo * 0.2;
+    titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 20% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+} else if (ageField.value == "over") { //Sconto del 40%
+    const nome = nameField.value;
+    const kmViaggio = tripField.value;
+    const etaPasseggero = ageField.value;
+    var prezzo = 0.21 * kmViaggio; 
+    prezzo -= prezzo * 0.4;
+    titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 40% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+}
+
+
+
