61d8196529e3d9df8ec6fae48606089dc21522d8
lufranski <lufranski@gmail.com>
2022-11-08 17:32:34 +0100

    InnerHTML Opt

--- js/main.js
+++ js/main.js
@@ -16,17 +16,29 @@ myButton.addEventListener("click",
         const kmViaggio = tripField.value;
         var prezzo = 0.21 * kmViaggio; 
         titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km, il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+
+        console.log("nome e cognome: " , nome);
+        console.log("durata viaggio: " , kmViaggio);
+        console.log("prezzo del biglietto: " , prezzo.toFixed(2));
         
         // Condizioni
 
         if (ageField.value == "under") { //Sconto del 20%
             const etaPasseggero = ageField.value;
             prezzo -= prezzo * 0.2;
-            titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 20% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+            const msgCaption = "e hai diritto ad uno sconto del 20% secondo la tariffa " + etaPasseggero;
+            titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km ${msgCaption}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+
+            console.log("tariffa: " , etaPasseggero);
+
         } else if (ageField.value == "over") { //Sconto del 40%
             const etaPasseggero = ageField.value;
             prezzo -= prezzo * 0.4;
-            titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 40% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+            const msgCaption = "e hai diritto ad uno sconto del 40% secondo la tariffa " + etaPasseggero;
+            titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km ${msgCaption}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+
+            console.log("tariffa: " , etaPasseggero);
+
         }
     }
 );
