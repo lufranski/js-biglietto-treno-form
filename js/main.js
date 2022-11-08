523811e76c488778bbe6d323e57ce71f54dae2a9
lufranski <lufranski@gmail.com>
2022-11-08 16:54:17 +0100

    JS Fix

--- js/main.js
+++ js/main.js
@@ -8,12 +8,26 @@ const ageField = document.getElementById("age");
 const myButton = document.querySelector("button");
 const myButtonReset = document.getElementById("reset");
 
+// Interazione con button Genera e Annulla
+
 myButton.addEventListener("click",
     function(){
         const nome = nameField.value;
         const kmViaggio = tripField.value;
         var prezzo = 0.21 * kmViaggio; 
         titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km, il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+        
+        // Condizioni
+
+        if (ageField.value == "under") { //Sconto del 20%
+            const etaPasseggero = ageField.value;
+            prezzo -= prezzo * 0.2;
+            titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 20% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+        } else if (ageField.value == "over") { //Sconto del 40%
+            const etaPasseggero = ageField.value;
+            prezzo -= prezzo * 0.4;
+            titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 40% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
+        }
     }
 );
 
@@ -23,24 +37,4 @@ myButtonReset.addEventListener("click",
         tripField.value = null;
     }
 );
-
-// Condizioni
-
-if (ageField.value == "under") { //Sconto del 20%
-    const nome = nameField.value;
-    const kmViaggio = tripField.value;
-    const etaPasseggero = ageField.value;
-    var prezzo = 0.21 * kmViaggio; 
-    prezzo -= prezzo * 0.2;
-    titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 20% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
-} else if (ageField.value == "over") { //Sconto del 40%
-    const nome = nameField.value;
-    const kmViaggio = tripField.value;
-    const etaPasseggero = ageField.value;
-    var prezzo = 0.21 * kmViaggio; 
-    prezzo -= prezzo * 0.4;
-    titolo.innerHTML = ` Ciao ${nome}, hai selezionato un viaggio da ${kmViaggio} km e hai diritto ad uno sconto del 40% secondo la tariffa ${etaPasseggero}, pertanto il prezzo del tuo biglietto sar&aacute; ${prezzo.toFixed(2)} &euro; `;
-}
-
-
 
